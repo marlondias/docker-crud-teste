c1e5edd7ee7093a8c7afdfa78e030b1ae91dc537
Marlon Dias <marlondias@users.noreply.github.com>
2021-07-11 14:00:20 -0300

    Correção na factory

--- backend/database/factories/DeveloperFactory.php
+++ backend/database/factories/DeveloperFactory.php
@@ -12,11 +12,14 @@ class DeveloperFactory extends Factory
     public function definition(): array
     {
         $isMale = $this->faker->boolean;
+        $hobbyActivity = $this->faker->randomElement(['Ciclismo', 'Caminhada', 'Artesanato', 'Fotografia']);
+        $hobbyStyle = $this->faker->randomElement(['radical', 'experimental', 'relaxante']);
+        $hobbyPlace = $this->faker->randomElement(['montanhas', 'desertos', 'florestas']);
     	return [
             'nome' => $this->faker->name($isMale ? 'male' : 'female'),
             'sexo' => $isMale ? 'M' : 'F',
             'idade' => $this->faker->numberBetween(0, 99),
-            'hobby' => $this->faker->words(),
+            'hobby' => "{$hobbyActivity} {$hobbyStyle} em {$hobbyPlace}",
             'data_nascimento' => $this->faker->date()
     	];
     }
